d71b8a57406cc37b6413c23fb6c27cc5c406bd1a
Shrivardhan567 <130844866+Shrivardhan567@users.noreply.github.com>
2023-10-12 19:42:36 +0530

    Update index.js solution1

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,35 +1,49 @@
 // Write your code here
 import {Component} from 'react'
+
 import './index.css'
 
 class ShowHide extends Component {
-  state = {fName: false, lName: false}
-
-  showFirstName = () => {
-    this.setState(prevState => ({fName: !prevState.fName}))
+  state = {
+    showFirstName: false,
+    showLastName: false,
   }
 
-  showLastName = () => {
-    this.setState(prevState => ({lName: !prevState.lName}))
+  onShowLastName = () => {
+    this.setState(prevState => ({showLastName: !prevState.showLastName}))
+  }
+
+  onShowFirstName = () => {
+    this.setState(prevState => ({showFirstName: !prevState.showFirstName}))
   }
 
   render() {
-    const {fName, lName} = this.state
+    const {showFirstName, showLastName} = this.state
 
     return (
-      <div>
-        <h1>Show/Hide</h1>
-        <div>
-          <button type="button" onClick={this.showFirstName}>
-            Show/Hide FirstName
-          </button>
-          {fName && <p className="name">Joe</p>}
-        </div>
-        <div>
-          <button type="button" onClick={this.showLastName}>
-            Show/Hide LastName
-          </button>
-          {lName && <p className="name">Jonas</p>}
+      <div className="app-container">
+        <h1 className="heading">Show/Hide</h1>
+        <div className="show-hide-container">
+          <div className="name-container">
+            <button
+              type="button"
+              className="show-hide-button"
+              onClick={this.onShowFirstName}
+            >
+              Show/Hide Firstname
+            </button>
+            {showFirstName && <p className="name">Joe</p>}
+          </div>
+          <div className="name-container">
+            <button
+              type="button"
+              className="show-hide-button"
+              onClick={this.onShowLastName}
+            >
+              Show/Hide Lastname
+            </button>
+            {showLastName && <p className="name">Jonas</p>}
+          </div>
         </div>
       </div>
     )
